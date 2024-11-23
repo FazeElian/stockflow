c3c74c2ff53cdc51557f582ab14339fc3f5c4ee1
FazeElian <elian.ibarra@soy.sena.edu.co>
2024-11-23 18:21:02 -0500

    Addition of app features section with detailed content of each one

--- client/src/views/IndexView.jsx
+++ client/src/views/IndexView.jsx
@@ -140,6 +140,119 @@ const IndexView = () => {
                     Empezar Ahora
                 </motion.a>
             </section>
+            <section className="app-features bg-black-medium font-inter">
+                <h1 className="color-white bg-transparent">Funcionalidades esenciales para tu negocio</h1>
+                <div className="items-app-features bg-transparent">
+                    <motion.div
+                        className="item-app-features bg-gray-dark"
+                        whileHover={{ scale: 1.1 }}
+                        transition={{
+                            duration: .25,
+                        }}
+                    >
+                        <div className="top-item-app-features bg-transparent">
+                            <img src={DashboardIcon} className="bg-transparent" alt="Dashboard" />
+                            <h2 className="color-white bg-transparent">Dashboard</h2>
+                        </div>
+                        <div className="btm-item-app-features bg-transparent">
+                            <p className="color-gray bg-transparent">
+                                Obtén una visión general de tu negocio con estadísticas de tu negocio,
+                                análisis detallados y reportes personalizados, todo en un solo lugar.
+                            </p>
+                        </div>
+                    </motion.div>
+                    <motion.div
+                        className="item-app-features bg-gray-dark"
+                        whileHover={{ scale: 1.1 }}
+                        transition={{
+                            duration: .25,
+                        }}
+                    >
+                        <div className="top-item-app-features bg-transparent">
+                            <img src={ProductsIcon} className="bg-transparent" alt="Productos" />
+                            <h2 className="color-white bg-transparent">Productos</h2>
+                        </div>
+                        <div className="btm-item-app-features bg-transparent">
+                            <p className="color-gray bg-transparent">
+                                Agregue, edite y organice fácilmente su catálogo de productos.
+                                Cada producto incluye detalles como código, nombre, categoría y precio.
+                            </p>
+                        </div>
+                    </motion.div>
+                    <motion.div
+                        className="item-app-features bg-gray-dark"
+                        whileHover={{ scale: 1.1 }}
+                        transition={{
+                            duration: .25,
+                        }}
+                    >
+                        <div className="top-item-app-features bg-transparent">
+                            <img src={CategoriesIcon} className="bg-transparent" alt="Categorías" />
+                            <h2 className="color-white bg-transparent">Categorías</h2>
+                        </div>
+                        <div className="btm-item-app-features bg-transparent">
+                            <p className="color-gray bg-transparent">
+                                Clasifica y organiza tus productos por categorías para facilitar la
+                                navegación y la gestión de tu inventario, haciéndolo más eficiente.
+                            </p>
+                        </div>
+                    </motion.div>
+                    <motion.div
+                        className="item-app-features bg-gray-dark"
+                        whileHover={{ scale: 1.1 }}
+                        transition={{
+                            duration: .25,
+                        }}
+                    >
+                        <div className="top-item-app-features bg-transparent">
+                            <img src={InventoriesIcon} className="bg-transparent" alt="Inventarios" />
+                            <h2 className="color-white bg-transparent">Inventarios</h2>
+                        </div>
+                        <div className="btm-item-app-features bg-transparent">
+                            <p className="color-gray bg-transparent">
+                                Administra tu inventario con precisión: realiza seguimientos detallados,
+                                evita faltantes y mantén siempre el control de tu stock.
+                            </p>
+                        </div>
+                    </motion.div>
+                    <motion.div
+                        className="item-app-features bg-gray-dark"
+                        whileHover={{ scale: 1.1 }}
+                        transition={{
+                            duration: .25,
+                        }}
+                    >
+                        <div className="top-item-app-features bg-transparent">
+                            <img src={SalesIcon} className="bg-transparent" alt="Ventas" />
+                            <h2 className="color-white bg-transparent">Ventas</h2>
+                        </div>
+                        <div className="btm-item-app-features bg-transparent">
+                            <p className="color-gray bg-transparent">
+                                Registra, supervisa y analiza tus ventas de forma intuitiva,
+                                asegurándote de maximizar tus ingresos y satisfacer a tus clientes.
+                            </p>
+                        </div>
+                    </motion.div>
+                    <motion.div
+                        className="item-app-features bg-gray-dark"
+                        whileHover={{ scale: 1.1 }}
+                        transition={{
+                            duration: .25,
+                        }}
+                    >
+                        <div className="top-item-app-features bg-transparent">
+                            <img src={CustomersIcon} className="bg-transparent" alt="Clientes" />
+                            <h2 className="color-white bg-transparent">Clientes</h2>
+                        </div>
+                        <div className="btm-item-app-features bg-transparent">
+                            <p className="color-gray bg-transparent">
+                                Gestiona la información de tus clientes de manera eficiente: 
+                                almacena datos clave, enlázalos a tus ventas e identifica patrones de compra.
+                            </p>
+                        </div>
+                    </motion.div>
+                </div>
+            </section>
         </main>
     )
 }
