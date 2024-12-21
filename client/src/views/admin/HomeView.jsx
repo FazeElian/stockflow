dffb9a9b8f0f7c3a3c2ffb6e25c65cfda649fdd5
FazeElian <elian.ibarra@soy.sena.edu.co>
2024-12-21 17:37:13 -0500

    add: Log out function

--- client/src/views/admin/HomeView.jsx
+++ client/src/views/admin/HomeView.jsx
@@ -1,8 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
 const HomeView = () => {
+    // Redirection
+    const navigate = useNavigate();
+    
+    const logOut = () => {
+        localStorage.removeItem("authToken");
+        navigate("/auth/login/");
+        alert("Has cerrado sesión con éxito");
+    }
+    
     return (
         <div>
             HomeView
-            <button>
+            <br />
+            <button onClick={logOut}>
                 Cerrar Sesión
             </button>
         </div>
