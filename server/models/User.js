e91ef3f419d12001a5cc8511ed8e8a74493772ea
FazeElian <elian.ibarra@soy.sena.edu.co>
2024-12-22 11:54:27 -0500

    add: Profile photo attribute for <User> & custom timestamps attributes

--- server/models/User.js
+++ server/models/User.js
@@ -1,26 +1,32 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../db");
 
-const User = sequelize.define("User", 
-    {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+const User = sequelize.define("User", {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
     },
-    {
-        timestamps: true,
-        tableName: 'USERS',
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
     },
-);
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    profilePhoto: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+    },
+});
 
 module.exports = User;
