7fd11392c14b3bff93618b0bfa1895b7f538ed55
FazeElian <elian.ibarra@soy.sena.edu.co>
2024-12-31 15:52:42 -0500

    Sending request with the JWT - headers

--- client/src/api/StockFlowAPI.ts
+++ client/src/api/StockFlowAPI.ts
@@ -4,9 +4,16 @@ import { isAxiosError } from "axios";
 import api from "../config/axios";
 
 export async function getUser () {
+    // Get token
+    const token = localStorage.getItem("AUTH_TOKEN");
+
     try {
-        const { data } = await api("/user");
-        console.log(data);
+        const { data } = await api("/user", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        return data;
     } catch (error) {
         if (isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error);
