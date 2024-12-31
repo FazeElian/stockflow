178a58051b0e27dcacac4bf524ebbd042c998cc4
FazeElian <elian.ibarra@soy.sena.edu.co>
2024-12-31 16:08:00 -0500

    add: JWT in an Axios Interceptor

--- client/src/api/StockFlowAPI.ts
+++ client/src/api/StockFlowAPI.ts
@@ -4,15 +4,8 @@ import { isAxiosError } from "axios";
 import api from "../config/axios";
 
 export async function getUser () {
-    // Get token
-    const token = localStorage.getItem("AUTH_TOKEN");
-
     try {
-        const { data } = await api("/user", {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
+        const { data } = await api("/user");
         return data;
     } catch (error) {
         if (isAxiosError(error) && error.response) {
--- client/src/config/axios.ts
+++ client/src/config/axios.ts
@@ -4,4 +4,15 @@ const api = axios.create ({
     baseURL: import.meta.env.VITE_API_URL
 })
 
+api.interceptors.request.use((config) => {
+    // Get token
+    const token = localStorage.getItem("AUTH_TOKEN");
+    if(token) {
+        // Set token on headers
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+})
+
 export default api;
