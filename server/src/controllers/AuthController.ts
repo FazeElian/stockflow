db16cfb70c70671acd038986f2261ad4578c00c9
FazeElian <elian.ibarra@soy.sena.edu.co>
2025-04-22 20:51:15 -0500

    add: Register funciton

--- server/src/controllers/AuthController.ts
+++ server/src/controllers/AuthController.ts
@@ -1,12 +1,36 @@
 import { Request, Response } from "express"
 
+// Model
+import User from "../models/User";
+
+// Utils for this controller
+import { hashPassword } from "../utils/auth";
+
 export class AuthController {
-    static login = async (req: Request, res: Response) => {
-        console.log("Login function")
+    static register = async (req: Request, res: Response) => {
+        const { email, password } = req.body;
+
+        // Check if the user already exists
+        const existingUser = await User.findOne({ where: { email } });
+
+        if(existingUser) {
+            const error = new Error("Este correo electrónico ya está asociado a otra cuenta.");
+            res.status(409).json({ error: error.message });
+            return;
+        }       
+
+        // Create new user
+        const user = new User(req.body);
+
+        // Hash password (for security)
+        user.password = await hashPassword(password)
+
+        // Save
+        await user.save();
     }
 
-    static register = async (req: Request, res: Response) => {
-        console.log("Register function")
+    static login = async (req: Request, res: Response) => {
+        console.log("Login function")
     }
 
     static confirmAccount = async (req: Request, res: Response) => {
