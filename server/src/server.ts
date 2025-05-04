4e982b70c27533a9a244efe95885a16ec5359bff
FazeElian <elian.ibarra@soy.sena.edu.co>
2025-05-04 10:34:27 -0500

    add: Server CORS config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,10 @@
 import express from 'express'
 import colors from "colors";
 import morgan from 'morgan'
+import cors from "cors";
+
+// CORS Config
+import { CORSConfig } from './config/cors';
 
 // Database config
 import { db } from "./config/db";
@@ -24,6 +28,7 @@ connectDB();
 
 const app = express()
 
+app.use(cors(CORSConfig))
 app.use(morgan('dev'))
 app.use(express.json())
 
