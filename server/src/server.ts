3b8a905d5531c987d8cf2542f712ed02ae1d6752
FazeElian <elian.ibarra@soy.sena.edu.co>
2025-06-15 12:44:30 -0500

    Initialize project

---
+++ server/src/server.ts
@@ -0,0 +1,27 @@
+import express from "express"
+import colors from "colors";
+import morgan from "morgan"
+
+// Database config
+import { db } from "./config/db";
+
+async function connectDB () {
+    try {
+        await db.authenticate();
+        db.sync();
+        console.log(colors.blue.bold("Connected to the database sucessfully"));
+    } catch (error) {
+        console.log(colors.red.bold(`Error while connecting to the database: ${error}`));
+    }
+}
+connectDB();
+
+const app = express()
+
+app.use(morgan("dev"))
+app.use(express.json())
+
+// API
+// app.use("/api/v1/");
+
+export default app
